5d61b5202d0f775bc24bc65a59d552e6adf1cbda
Liangzhihe <1789211411@qq.com>
2019-09-30 10:27:37 +0800

    update

--- JavaScript/temp.js
+++ JavaScript/temp.js
@@ -1,26 +1,26 @@
-for (var i = 0; i < 5; i++) {
-  setTimeout(() => {
-    console.log(i)
-  }, 100)
+const arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
 
+// 编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
+// const list = []
+function getArray(array, list) {
+  array.forEach(item => {
+    // console.log(typeof item)
+    if (Array.isArray(item)) {
+      getArray(item, list)
+    } else {
+      if (!list.includes(item)) {
+        list.push(item)
+      }
+    }
+  })
 }
 
-for (var i = 0; i < 5; i++) {
-  (function (i) {
-    setTimeout(() => {
-      console.log(i)
-    }, 100)
-  })(i)
+function fliteArray(array) {
+  const list = []
+  getArray(array, list)
+  list.sort((a,b) => a - b)
+  return list
 }
 
-var data = [1, [2, [
-  [3, [7, 8], 4], 5
-], 6]]
-
-function flatten(arr) {
-  while (arr.some(item => Array.isArray(item))) {
-    arr = [].concat(...arr);
-  }
-  return arr;
-}
-console.log(flatten(data).sort())
+const a = fliteArray(arr)
+console.log(a)
